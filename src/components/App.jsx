0cbc00f43176c778daa67d37c8902b47f5a69d4d
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-05 19:59:13 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,61 @@
 import axios from 'axios';
 import './App.css';
 import { SearchBar } from './SearchBar/SearchBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { ErrorMassage } from './ErrorMessage/ErrorMassage';
+import { LoadMoreBtn } from './LoadMoreBtn/LoadMoreBtn';
 
 export const App = () => {
   const accessKey = 'I_SGforXj0P7ovqaJ6GJpJk2xEwutBiTFEwpgZcNd1M';
 
   const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const searchImages = async query => {
-    try {
-      setError(false);
-      setLoading(true);
-      const response = await axios.get(
-        `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`
-      );
-      setPhotos(response.data.results);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
+    setQuery(query);
+    setPage(1);
+    setPhotos([]);
+    setLoading(false);
+    setError(false);
   };
-  console.log(photos);
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    async function fetchData() {
+      try {
+        setError(false);
+        setLoading(true);
+        const response = await axios.get(
+          `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}&page=${page}`
+        );
+        setPhotos(prevPhotos => [...prevPhotos, ...response.data.results]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [query, page]);
 
   return (
     <div>
       <SearchBar onSearch={searchImages} />
       {photos.length > 0 && <ImageGallery photos={photos} />}
       {loading && <Loader />}
-      {error && <ErrorMassage/>}
+      <LoadMoreBtn handleLoadMore={handleLoadMore} />
+      {error && <ErrorMassage />}
     </div>
   );
 };
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,7 @@
+export const LoadMoreBtn = ({ handleLoadMore }) => {
+    return (
+        <div>
+            <button onClick={handleLoadMore}>Load more</button>
+        </div>
+    )
+}
